430ff6ebd5e79d3f6cdfab996088060c57ee0593
Mikhail Gubin <MVGubin@mail.ru>
2024-12-11 21:22:55 +0300

    add function sort_by_date in src\processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,10 +1,12 @@
 def filter_by_state(operations_info: list, state: str = "EXECUTED") -> list:
-    """ "
-    Возвращает новый список словарей, в которых ключ state
-    содержит переданное в функцию значение
     """
-    list_of_operations = []
-    for operation in operations_info:
-        if operation["state"] == state:
-            list_of_operations.append(operation)
-    return list_of_operations
+    Возвращает новый список словарей, в которых ключ state содержит переданное в функцию значение
+    """
+    return [operation for operation in operations_info if operation["state"] == state]
+
+
+def sort_by_date(operations_info: list, increase: bool = True) -> list:
+    """
+    Возвращает новый список, отсортированный по дате в зависимости от значения параметра increase
+    """
+    return sorted(operations_info, key=lambda operation: operation["date"], reverse=not increase)

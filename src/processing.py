2ab5027aa5f38ec5f27c5e9ec40d41ea960247fc
Mikhail Gubin <MVGubin@mail.ru>
2024-12-12 20:25:11 +0300

    rename lists and variables in src\processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,12 @@
-def filter_by_state(operations_info: list, state: str = "EXECUTED") -> list:
+def filter_by_state(banking_transactions: list, state: str = "EXECUTED") -> list:
     """
     Возвращает новый список словарей, в которых ключ state содержит переданное в функцию значение
     """
-    return [operation for operation in operations_info if operation["state"] == state]
+    return [transaction for transaction in banking_transactions if transaction["state"] == state]
 
 
-def sort_by_date(operations_info: list, increase: bool = True) -> list:
+def sort_by_date(banking_transactions: list, decreasing: bool = True) -> list:
     """
     Возвращает новый список, отсортированный по дате в зависимости от значения параметра increase
     """
-    return sorted(operations_info, key=lambda operation: operation["date"], reverse=increase)
+    return sorted(banking_transactions, key=lambda transaction: transaction["date"], reverse=decreasing)

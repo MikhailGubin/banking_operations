bca94fb88ce072870e03d0f5dea5687f74a64cfc
Mikhail Gubin <MVGubin@mail.ru>
2024-12-20 23:28:33 +0300

    modified masks.py, test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: str|int) -> str:
     """Маскирует номер банковской карты."""
     card_number_str = str(card_number)
-    if not isinstance(card_number_str, str|int) or len(card_number_str) != 16:
+    if not card_number_str.isdigit() or len(card_number_str) != 16:
         raise ValueError("Неправильно введен номер карты")
     return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[-4:]}"
 
@@ -9,6 +9,6 @@ def get_mask_card_number(card_number: str|int) -> str:
 def get_mask_account(account_number: str|int) -> str:
     """Маскирует номер банковского счета."""
     account_number_str = str(account_number)
-    if not isinstance(account_number_str, str | int) or len(account_number_str) != 16:
+    if not account_number_str.isdigit() or len(account_number_str) != 20:
         raise ValueError("Неправильно введен номер карты")
     return "**" + account_number_str[-4:]

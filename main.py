94ba5840033418a79e5926060ca14e5dc2177726
Mikhail Gubin <mgubin82@gmail.com>
2025-01-19 14:01:40 +0300

    Add directory logs to GitHub Small changes in module main.py and in file .gitignore

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import pprint
 
+from src.masks import get_mask_card_number, get_mask_account
+from src.utils import read_json_file, PATH_TO_FILE
+
 from data.data_for_main import bank_card_or_account_number, banking_operations_info, transactions_for_generate
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
+    # Visa Platinum 8990922113665229 - пример банковской карты
+    print(get_mask_card_number(8990922113665229))
+    # Счет 11776614605963066702 - пример банковского счета
+    print(get_mask_account(11776614605963066702))
+    print(read_json_file(PATH_TO_FILE))
 
     print(mask_account_card(bank_card_or_account_number))
     print(get_date("2019-07-03T18:35:29.512364"))
-
     pprint.pprint(filter_by_state(banking_operations_info), width=85, indent=4)
     pprint.pprint(sort_by_date(banking_operations_info, decreasing=False), width=85, indent=4)
 

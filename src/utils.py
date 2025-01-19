4d5532e27fd41894f7c03546bba60877c26d73e7
Mikhail Gubin <mgubin82@gmail.com>
2025-01-19 13:38:33 +0300

    Add logger in module utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,35 +1,56 @@
 import json
 import os
+import logging
 
-PATH_TO_FILE = os.path.join(os.path.dirname(__file__), "..", "data", "operations.json")
+
+# Получаю абсолютный путь к корневой директории проекта
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Задаю путь к файлу с транзакциями в формате JSON
+PATH_TO_FILE = os.path.join(BASE_DIR, "data", "operations.json")
+# Задаю путь к файлу utils.log в директории logs
+LOG_PATH =os.path.join(BASE_DIR, 'logs', 'utils.log')
 
+
+logging.basicConfig(
+    filename = LOG_PATH,
+    filemode= 'w',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
 
 def read_json_file(path: str) -> list:
     """
     Возвращает список словарей с данными о финансовых транзакциях из
     JSON-файла
     """
+    logger.info('Начало работы функции read_json_file')
     try:
         with open(path) as json_file:
 
             try:
                 transactions_list = json.load(json_file)
             except json.JSONDecodeError:
+                logger.error('Невозможно декодировать JSON-данные')
                 print("\nНевозможно декодировать JSON-данные")
                 return []
 
     except FileNotFoundError:
+        logger.error('JSON-файл не найден')
         print("\nФайл не найден")
         return []
-    else:
-        if not transactions_list:
-            print("\nФайл содержит пустой список")
-            return []
-        elif type(transactions_list) is not list:
-            print("\nТип объекта в файле не список")
-            return []
-        return transactions_list
+
+    if not transactions_list:
+        logger.error('JSON-файл содержит пустой список')
+        print("\nФайл содержит пустой список")
+        return []
+    elif type(transactions_list) is not list:
+        logger.error('Тип объекта в JSON-файле не список')
+        print("\nТип объекта в файле не список")
+        return []
+    logger.info('Функции read_json_file успешно отработала')
+    return transactions_list
 
 
 if __name__ == "__main__":
